4ec2bf18e1a04305a766cda423ef9764b9e29f1b
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2021-11-13 21:21:46 +0900

    removed unused script [skip ci]

--- scripts/build-unix.sh
+++
@@ -1,6 +0,0 @@
-# executable with test
-cmake -S . -B build/test -G Ninja -DCMAKE_C_COMPILER=clang-12 -DCMAKE_CXX_COMPILER=clang++-12 -DCMAKE_CXX_FLAGS="-Weverything -Wno-c++98-c++11-c++14-compat -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++20-compat -ftime-trace" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCPM_SOURCE_CACHE=~/dev/.cache/CPM -DBUILD_WITH_TEST=ON
-cmake --build build/test && ./build/test/minimalcpppj
-# executable without test
-cmake -S . -B build/notest -G Ninja -DCMAKE_C_COMPILER=clang-12 -DCMAKE_CXX_COMPILER=clang++-12 -DCMAKE_CXX_FLAGS="-Weverything -Wno-c++98-c++11-c++14-compat -Wno-c++98-compat -Wno-c++98-compat-pedantic -Wno-c++20-compat -ftime-trace" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCPM_SOURCE_CACHE=~/dev/.cache/CPM
-cmake --build build/notest && ./build/notest/minimalcpppj
