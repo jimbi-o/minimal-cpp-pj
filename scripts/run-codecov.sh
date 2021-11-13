27ed75fdaadeed22f92e9eae23427e45d49af19c
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2021-11-13 14:50:15 +0900

    changed codecov to use build directory

--- scripts/run-codecov.sh
+++ scripts/run-codecov.sh
@@ -1,4 +1,4 @@
-lcov --directory . --capture --output-file coverage.info
+lcov --directory build --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' "${HOME}"'/.cache/*' "${HOME}"'/build/_deps' --output-file coverage.info
 lcov --list coverage.info
 bash <(curl -s https://codecov.io/bash) -f coverage.info || echo "Codecov did not collect coverage reports"
